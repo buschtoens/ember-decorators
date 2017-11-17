b660134b02c95310b19c3a9a3699c1e7f54225d3
Jan Buschtöns <jan@buschtoens.me>
2017-11-17 12:24:29 +0100

    refactor: extract @ember-decorators/babel-transforms

---
+++ blueprints/ember-decorators/index.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  description: 'Adds @ember-decorators/babel-transforms to the parent app',
+
+  normalizeEntityName() {}, // no-op since we're just adding dependencies
+
+  afterInstall() {
+    // return this.addAddonToProject('@ember-decorators/babel-transforms');
+    return this.addAddonToProject('@ember-decorators/babel-transforms@ember-decorators/babel-transforms');
+  }
+};
